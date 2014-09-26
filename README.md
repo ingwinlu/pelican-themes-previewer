ce5a507680259b04b6c0c7568613154abb187aed
winlu <derwinlu+git@gmail.com>
2014-09-26 21:38:46 +0200

    update makefile, update config, update gitignore, add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+pelican-themes-previewer
+==========================
+this contains a pelican-theme and plugin to parse pelican-themes to generate a nice preview page.
+
+##requirements
+*   python-sh
--- pelicanconf.py
+++ pelicanconf.py
@@ -3,31 +3,30 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'pelican'
-SITENAME = 'pelican-theme previews'
-SITEURL = ''
+SITENAME = 'pelican-themes'
+SITEURL = 'http://dev.heroicdebugging.biz'
 
 PATH = 'content'
 
 TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = 'en'
+DEFAULT_DATE = 'fs'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
 DEFAULT_PAGINATION = 1
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+PLUGIN_PATHS = ["plugins"]
+PLUGINS = ["themes_git_reader"] #enter git reader/parser here
+
+THEME = "theme"
+DIRECT_TEMPLATES = ('index',)
+BOOTSTRAP_THEME = 'paper'
+PYGMENTS_STYLE = 'colorful'
