a8fac45a300ea9db8bf9b8e39f2b17228b6af6ee
winlu <derwinlu+git@gmail.com>
2014-09-28 12:00:25 +0200

    remove dropdown, add archives

--- pelicanconf.py
+++ pelicanconf.py
@@ -36,7 +36,7 @@ BOOTSTRAP_THEME = 'paper'
 PYGMENTS_STYLE = 'colorful'
 
 
-DIRECT_TEMPLATES = ('index',)
+DIRECT_TEMPLATES = ('index', 'archives')
 ARTICLE_URL = 'themes/{slug}/'
 ARTICLE_SAVE_AS = 'themes/{slug}/index.html'
 AUTHOR_SAVE_AS = ''
